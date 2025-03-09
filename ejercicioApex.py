adbe3d9fc137c5b52d20cdfc25bda87bb84b3f55
IsabellaAgudelo01 <isabella.agudelo01@usa.edu.co>
2025-03-09 13:38:50 -0500

    Update ejercicioApex.py

--- ejercicioApex.py
+++ ejercicioApex.py
@@ -1,21 +1,21 @@
 #Se le pide al usuario que ingrese el numero del circulo en el que está
-c = int(input("Escriba el círculo en el que se encuentra: "))
+c= int(input("Escriba el círculo en el que se encuentra: "))
 #Se le pide al usuario que ingrese el tiempo en el que estuvo en el círculo
-t = float(input("Escriba el tiempo en el que estuvo en el círculo: "))
-
-#Se evalua en que circulo está el usuario y se calcula el daño recibido
-if c == 1: 
-    a = 2*t #Si el circulo es 1, se multiplica el tiempo por 2
-    print (f"Daño total recibido: {a}") #Se imprime el daño total recibido
-elif c == 2:
-    b = 5*t #Si el circulo es 2, se multiplica el tiempo por 5
-    print (f"Daño total recibido: {b}") #Se imprime el daño total recibido
-elif c == 3:
-    d = 10*t #Si el circulo es 3, se multiplica el tiempo por 10
-    print (f"Daño total recibido: {d}") #Se imprime el daño total recibido
-elif c >= 4:
-    e = 25*t #Si el circulo es 4 o mas, se multiplica el tiempo por 25
-    print(f"Daño total recibido: {e}") #Se imprime el daño total recibido
-else: 
-    #Para casos en los que el circulo sea menor a 1
-    print("Usted no ha recibido daño") 
+t=float(input("Escriba el tiempo en el que estuvo en el círculo: "))
+#Se verifica el tipo de valor que se ingreso para el tiempo
+if t >= 0:
+    #Se evalua en que circulo está el usuario y se calcula el daño recibido
+    if c == 1:
+        print(f"Daño total recibido: {2 * t}") #Si el circulo es 1, se multiplica el tiempo por 2 y se imprime el valor recibido 
+    elif c == 2:
+        print(f"Daño total recibido: {5 * t}") #Si el circulo es 2, se multiplica el tiempo por 5 y se imprime el valor recibido 
+    elif c == 3:
+        print(f"Daño total recibido: {10 * t}") #Si el circulo es 3, se multiplica el tiempo por 10 y se imrprime el valor recibido 
+    elif c >= 4:
+        print(f"Daño total recibido: {25 * t}") #Si el circulo es 4 o mas, se multiplica el tiempo por 25 y se imprime el valor recibido
+    elif c == 0:
+        print("Usted no ha recibido daño") #Para casos en los que el circulo sea igual a 0
+    else:
+        print("Por favor ingrese el círculo en el que se encuentra como un entero positivo") #Para casos en el que el círculo no sea coherente o no esté en el formato requerido
+else:
+    print("Por favor ingrese el tiempo como un real positivo") #Para casos en el que el tiempo no sea coherente o no esté en el formate requerido 
