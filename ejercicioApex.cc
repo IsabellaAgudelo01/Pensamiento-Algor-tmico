58d9e3866cd134545b102a5d71906c8e4e4fae57
IsabellaAgudelo01 <isabella.agudelo01@usa.edu.co>
2025-03-10 06:55:01 -0500

    Update ejercicioApex.cc

--- ejercicioApex.cc
+++ ejercicioApex.cc
@@ -1,32 +1,32 @@
 #include <iostream> //Biblioteca que permite la entrada y salida de datos
 
-int c; //Variable que almacena el círculo en el que se encuentra
-float t; //Variable que almacena el tiempo en el que estuvo en el círculo
+int c; 
+float t; 
 int main (){
     //Se le pide al usuario que ingrese el numero del circulo en el que está 
     std::cout << "Escriba el círculo en el que se encuentra" << std::endl;
     std::cin >> c;
-    //Se le pide al usuario que ingrese el tiempo en el que estuvo en el círculo
+    //Se le pide al usuario que ingrese el tiempo en segundos en el que estuvo en el círculo
     std::cout << "Escriba el tiempo en segundos en el que estuvo en el círculo" << std::endl;
     std::cin >> t;
-    //Se evalúa que el tiempo ingresado ssea coherente
+    //Se evalúa que el tiempo ingresado sea coherente
    if (t>=0){
        //Se evalua en que circulo está el usuario 
           if(c==1){
-         float a = 2*t; //Si el circulo es 1, se multiplica el tiempo por 2
-         std::cout << "Daño total recibido: " << a << std::endl; //Se imprime el daño total recibido
+         float a = 2*t; 
+         std::cout << "Daño total recibido: " << a << std::endl; 
      } else {
          if(c==2){
-             float b = 5*t; //Si el circulo es 2, se multiplica el tiempo por 5
-             std::cout << "Daño total recibido: " << b << std::endl; //Se imprime el daño total recibido
+             float b = 5*t; 
+             std::cout << "Daño total recibido: " << b << std::endl; 
          } else {
              if(c==3){
                  float d = 10*t;  //Si el circulo es 3, se multiplica el tiempo por 10
-                 std::cout << "Daño total recibido: " << d << std::endl; //Se imprime el daño total recibido
+                 std::cout << "Daño total recibido: " << d << std::endl; 
              } else {
                  if(c>=4){
-                     float e = 25*t;  //Si el circulo es 4 o mas, se multiplica el tiempo por 25
-                     std::cout << "Daño total recibido: " << e << std::endl; //Se imprime el daño total recibido
+                     float e = 25*t;  
+                     std::cout << "Daño total recibido: " << e << std::endl; 
                  } else{
                      if (c==0){
                          //Para casos en los que el circulo sea igual a 0
